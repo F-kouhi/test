b7391b13652b962549286f100dc968b1e105a881
F.kouhi <fa.kouhii@gmail.com>
2024-06-15 17:12:52 +0330

    basic

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <iostream>
+using namespace std;
+
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* data) {
+    data->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+
+    cout << "hello\n";
+
+    return a.exec();
+}
