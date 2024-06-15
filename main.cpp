4d881fe9cd82192a4a8c8e5797ee19d3a3da8203
F.kouhi <fa.kouhii@gmail.com>
2024-06-15 17:47:04 +0330

    added comments to the pro files.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include "mainwindow.h"
 #include <iostream>
 using namespace std;
 
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* data) {
-    data->append((char*)contents, size * nmemb);
+size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *data)
+{
+    data->append((char *) contents, size * nmemb);
     return size * nmemb;
 }
 
@@ -14,8 +15,8 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-
     cout << "hello\n";
+    cout << "bye\n";
 
     return a.exec();
 }
